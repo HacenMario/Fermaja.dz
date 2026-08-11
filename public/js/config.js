3dd36d26005eb80b500a66efd0495dc08ad65792
HacenMario <stevenhacen@gmail.com>
2026-08-11 02:03:02 +0100

    Update API_BASE URL for production environment

--- public/js/config.js
+++ public/js/config.js
@@ -1,6 +1,6 @@
 // ===== تعريف API_BASE =====
 window.API_BASE = window.location.hostname === 'localhost' 
   ? '' 
-  : 'https://fermaja-dz.onrender.com';
+  : 'https://fermaja-dz-nhsn.onrender.com';
 
 console.log('🌐 config.js: API_BASE =', window.API_BASE);
